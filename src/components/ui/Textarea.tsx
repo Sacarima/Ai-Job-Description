8fe7af1c21b4fe8af8e24352238e7e61469a4cdf
Sacarima <caroline12alex@yahoo.com>
2025-12-03 09:26:39 +0100

    featuring: fix  iOS zooms in when focusing on input

--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -7,7 +7,7 @@ type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
 export function Textarea({ className, ...props }: TextareaProps) {
   const base =
     "block w-full rounded-md border border-input bg-card " +
-    "px-5 py-2 text-sm text-card-foreground shadow-sm " +
+    "px-5 py-2 text-base sm:text-sm text-card-foreground shadow-sm " +
     "placeholder:text-muted-foreground focus:outline-none focus:ring-2 " +
     "focus:ring-ring focus:border-ring";
 
